ce8cc1633330aca6d440e5d9bdd2d20ca2751b40
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-01-16 00:06:03 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let number = parseInt(count);
+    if (number <= 0) {
+      return (number = 1);
+    }
+    if (number > data.length - 1) {
+      return (number = data.length - 1);
+    }
+    setText(data.slice(0, number));
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      {" "}
+      <h3>tired of boring lorem ipsum?</h3>
+      <form onSubmit={submitHandler} className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, idx) => {
+          return <p key={idx}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
